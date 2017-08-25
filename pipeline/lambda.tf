cffc748e764a2aaced270c30f626e0b6928a7494
14mar1983 <14mar1983@gmail.com>
2017-08-25 11:44:58 +0000

    Update lambda.tf

--- pipeline/lambda.tf
+++ pipeline/lambda.tf
@@ -8,6 +8,6 @@ resource "aws_lambda_function" "_airflow_request" {
   memory_size = 128
   role = "${aws_iam_role.lambda_execution_role.arn}"
   runtime = "python3.6.0"
-  source_code_hash = "${var.lambda_file}.output_base64sha256}"
+  source_code_hash = "${base64sha256(file(var.lambda_file))}"
 }
 
