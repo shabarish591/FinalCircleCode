5b36b1a79a2b386f4280f70ddd31cc540b67d7a1
14mar1983 <14mar1983@gmail.com>
2017-08-25 07:36:15 +0000

    Update lambda.tf

--- pipeline/lambda.tf
+++ pipeline/lambda.tf
@@ -8,5 +8,5 @@ resource "aws_lambda_function" "api_lambda_function" {
   memory_size = 128
   role = "${aws_iam_role.lambda_execution_role.arn}"
   runtime = "python3.6.0"
-  source_code_hash = "${base64sha256(file(var.lambda_file))}"
+  source_code_hash = "${base64sha256(Lambda-build.zip)}"
 }
