d1b81fdcd9dceec8ce0fffb2ff9420e63d90b529
14mar1983 <14mar1983@gmail.com>
2017-08-25 07:59:56 +0000

    Update lambda.tf

--- pipeline/lambda.tf
+++ pipeline/lambda.tf
@@ -1,12 +1,13 @@
 
 #Lambda function
 resource "aws_lambda_function" "api_lambda_function" {
-  filename = "Lambda-build.zip"
+  filename = "${var.lambda_file}"
   function_name = "${var.lambda_name}"
   handler = "switchboard.handler"
   timeout = 5
   memory_size = 128
   role = "${aws_iam_role.lambda_execution_role.arn}"
   runtime = "python3.6.0"
-  source_code_hash = "${base64sha256(file("Lambda-build.zip"))}"
+  source_code_hash = "${base64sha256(file(var.lambda_file))}"
 }
+
