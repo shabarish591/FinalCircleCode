c627f1dc6f7be386b7133b6e2a2319061845ac53
14mar1983 <14mar1983@gmail.com>
2017-08-25 08:05:43 +0000

    Update lambda.tf

--- pipeline/lambda.tf
+++ pipeline/lambda.tf
@@ -1,6 +1,6 @@
 
 #Lambda function
-resource "aws_lambda_function" "api_lambda_function" {
+resource "aws_lambda_function" "_airflow_request" {
   filename = "${var.lambda_file}"
   function_name = "${var.lambda_name}"
   handler = "switchboard.handler"
@@ -8,6 +8,6 @@ resource "aws_lambda_function" "api_lambda_function" {
   memory_size = 128
   role = "${aws_iam_role.lambda_execution_role.arn}"
   runtime = "python3.6.0"
-  source_code_hash = "${base64sha256(file(var.lambda_file))}"
+  source_code_hash = "${var.lambda_file}.output_base64sha256}"
 }
 
