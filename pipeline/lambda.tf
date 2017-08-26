410a8e66fb37aba34b1858c3bae034a292a437e8
shabu591 <vjshabas@gmail.com>
2017-08-26 02:12:10 -0400

    updates

--- pipeline/lambda.tf
+++ pipeline/lambda.tf
@@ -1,13 +1,49 @@
-
-#Lambda function
-resource "aws_lambda_function" "_airflow_request" {
-  filename = "lambda-build.zip"
-  function_name = "${var.lambda_name}"
-  handler = "switchboard.handler"
-  timeout = 5
-  memory_size = 128
-  role = "${aws_iam_role.lambda_execution_role.arn}"
-  runtime = "python3.6.0"
-  source_code_hash = "${base64sha256(file(lambda-build.zip))}"
+provider "aws" {
+	region = "us-east-1"
 }
+
+resource "aws_lambda_function" "_airflow_request" {
+  filename = "${var.lambda_file}"
+  function_name = "_airflow_request"
+  handler = "switchboard.handler"
+  runtime = "python3.6"
+  role = "${aws_iam_role.lambda_execution_role.arn}"
+  source_code_hash = "${base64sha256(file(var.lambda_file))}"
+}
+
+resource "aws_lambda_function" "_em7_message_check_file_system" {
+  filename = "${var.lambda_file}"
+  function_name = "_em7_message_check_file_system"
+  handler = "switchboard.handler"
+  runtime = "python3.6"
+  role = "${aws_iam_role.lambda_execution_role.arn}"
+  source_code_hash = "${base64sha256(file(var.lambda_file))}"
+}
+
+resource "aws_lambda_function" "_get_config_dict" {
+  filename = "${var.lambda_file}"
+  function_name = "_get_config_dict"
+  handler = "switchboard.handler"
+  runtime = "python3.6"
+  role = "${aws_iam_role.lambda_execution_role.arn}"
+  source_code_hash = "${base64sha256(file(var.lambda_file))}"
+}
+
+resource "aws_iam_role" "lambda_execution_role" {
+ assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
 
--- pipeline/variables.tf
+++ pipeline/variables.tf
@@ -1,36 +1,6 @@
-variable "region" {
-  default = "us-west-2"
-}
-
-data "aws_caller_identity" "current" {}
-
-provider "aws" {
-  region = "${var.region}"
-  access_key = "AKIAJLLVUAZ6XKN3OFGA"
-  secret_key = "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"
-}
-
-variable "stage" {
-  default = "dev"
-}
-
 variable "lambda_file" {
-  default = "lambda-build.zip"
+  default = "../pipeline/lambda.zip"
 }
-
-variable "lambda_name" {
-  default = "_airflow_request"
-}
-
-variable "api_gateway_name" {
-  default = "MyCircleProxy"
-}
-
-terraform remote config {
-  backend "s3" {
-    bucket = "serverless-lambda-b"
-    key    = "circle_demo"
-    region = "us-west-2"
-    profile = "sabarish" 
-  }
+variable "role_name" {
+  default = "SampleCircleRole"
 }
