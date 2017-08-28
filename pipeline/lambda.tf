849a697b05c9f3b14fd5074400f7b8188e139e13
shabarish591 <shabu9993@gmail.com>
2017-08-27 19:10:41 -0700

    Update lambda.tf

--- pipeline/lambda.tf
+++ pipeline/lambda.tf
@@ -2,27 +2,9 @@ provider "aws" {
 	region = "us-east-1"
 }
 
-resource "aws_lambda_function" "_airflow_request" {
+resource "aws_lambda_function" "Test_airflow_request" {
   filename = "${var.lambda_file}"
   function_name = "_airflow_request"
-  handler = "switchboard.handler"
-  runtime = "python3.6"
-  role = "${aws_iam_role.lambda_execution_role.arn}"
-  source_code_hash = "${base64sha256(file(var.lambda_file))}"
-}
-
-resource "aws_lambda_function" "_em7_message_check_file_system" {
-  filename = "${var.lambda_file}"
-  function_name = "_em7_message_check_file_system"
-  handler = "switchboard.handler"
-  runtime = "python3.6"
-  role = "${aws_iam_role.lambda_execution_role.arn}"
-  source_code_hash = "${base64sha256(file(var.lambda_file))}"
-}
-
-resource "aws_lambda_function" "_get_config_dict" {
-  filename = "${var.lambda_file}"
-  function_name = "_get_config_dict"
   handler = "switchboard.handler"
   runtime = "python3.6"
   role = "${aws_iam_role.lambda_execution_role.arn}"
