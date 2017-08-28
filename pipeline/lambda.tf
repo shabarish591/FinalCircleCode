7a5d075b445f032d198fb3a3bc56318a8d13d368
shabarish591 <shabu9993@gmail.com>
2017-08-28 14:02:26 -0700

    Update lambda.tf

--- pipeline/lambda.tf
+++ pipeline/lambda.tf
@@ -7,7 +7,7 @@ resource "aws_lambda_function" "Test_airflow_request" {
   function_name = "T_airflow_request"
   handler = "switchboard.handler"
   runtime = "python2.7"
-  role = "${aws_iam_role.lambda_execution_role.arn}"
+  #role = "${aws_iam_role.lambda_execution_role.arn}"
   source_code_hash = "${base64sha256(file(var.lambda_file))}"
 }
 
