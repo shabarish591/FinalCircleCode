71fc25736edd1caa6027d584668bee8bf85994df
shabarish591 <shabu9993@gmail.com>
2017-08-28 14:00:19 -0700

    Update lambda.tf

--- pipeline/lambda.tf
+++ pipeline/lambda.tf
@@ -11,21 +11,6 @@ resource "aws_lambda_function" "Test_airflow_request" {
   source_code_hash = "${base64sha256(file(var.lambda_file))}"
 }
 
-resource "aws_iam_role" "lambda_execution_role" {
- assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow"
-    }
-  ]
-}
-EOF
-}
+
 
 
