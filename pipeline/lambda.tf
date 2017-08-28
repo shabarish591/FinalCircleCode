65f1d5ada17f5d7582c518eb511cddfd355fb4db
shabarish591 <shabu9993@gmail.com>
2017-08-27 19:19:49 -0700

    Update lambda.tf

--- pipeline/lambda.tf
+++ pipeline/lambda.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-	region = "us-east-1"
+	region = "us-west-2"
 }
 
 resource "aws_lambda_function" "Test_airflow_request" {
   filename = "${var.lambda_file}"
   function_name = "T_airflow_request"
   handler = "switchboard.handler"
-  runtime = "python3.6"
+  runtime = "python2.7"
   role = "${aws_iam_role.lambda_execution_role.arn}"
   source_code_hash = "${base64sha256(file(var.lambda_file))}"
 }
