0f53e257c0985a6abfd25ac4d3ace11748dd682b
shabarish591 <shabu9993@gmail.com>
2017-08-29 09:36:48 -0700

    Update lambda.tf

--- pipeline/lambda.tf
+++ pipeline/lambda.tf
@@ -2,15 +2,33 @@ provider "aws" {
 	region = "us-west-2"
 }
 
-resource "aws_lambda_function" "Test_airflow_request" {
+resource "aws_lambda_function" "T_airflow_request" {
   filename = "${var.lambda_file}"
-  function_name = "T_airflow_request"
+  function_name = "Test_airflow_request"
   handler = "switchboard.handler"
-  runtime = "python2.7"
-  #role = "${aws_iam_role.lambda_execution_role.arn}"
+  runtime = "python3.6"
+  role = "${aws_iam_role.lambda_exec_role.arn}"
   source_code_hash = "${base64sha256(file(var.lambda_file))}"
+ }
+
+
+resource "aws_iam_role" "lambda_exec_role" {
+	 name = "lambda_exec_role"
+ assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
 }
-
-
+EOF
+}
 
 
